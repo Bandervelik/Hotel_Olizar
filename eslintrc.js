8e5c5230b481f5cade20d2dd7426241d6449fa65
bandervelik <83076346+Bandervelik@users.noreply.github.com>
2024-10-13 21:53:11 +0300

    Second

---
+++ eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module', 
+      ecmaFeatures: {
+        jsx: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'airbnb',
+      'prettier',
+    ],
+    plugins: [
+      'react',
+      '@typescript-eslint',
+      'prettier',
+    ],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
+  
