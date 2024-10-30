0103604c25d68f7d37f9427c0ac6bba83ce6aa56
Olizar Mihailo <83076346+Bandervelik@users.noreply.github.com>
2024-10-30 20:53:37 +0000

    rd elone

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-const App = () => <h1>Hello, Webpack and Babel!</h1>;
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
